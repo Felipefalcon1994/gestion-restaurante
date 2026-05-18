89572f346bd489f7cc6bb6fbaaadd43230c48555
jorge cañas <jor.canas@duocuc.cl>
2026-05-18 19:31:09 -0400

    ahora si:C

--- menu/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT IGNORE INTO categorias (id_categoria, nombre, descripcion) VALUES (1, 'Platos Principales', 'Comida fuerte');
-INSERT IGNORE INTO categorias (id_categoria, nombre, descripcion) VALUES (2, 'Bebidas', 'Bebidas frias y calientes');
-
-INSERT IGNORE INTO productos (id_producto, nombre, precio, disponible, id_categoria) VALUES (1, 'Hamburguesa Clasica', 5000, true, 1);
-INSERT IGNORE INTO productos (id_producto, nombre, precio, disponible, id_categoria) VALUES (2, 'Jugo Natural', 2000, true, 2);
